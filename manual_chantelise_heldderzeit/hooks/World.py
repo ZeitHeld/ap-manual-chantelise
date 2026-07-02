f9fb63f22c7fedd9f5e9cf6cc82572381d0698fb
Held_der_Zeit <76132257+ZeitHeld@users.noreply.github.com>
2026-07-02 20:02:32 +0200

    IT WORKS

--- manual_chantelise_heldderzeit/hooks/World.py
+++ manual_chantelise_heldderzeit/hooks/World.py
@@ -125,28 +125,28 @@ def before_create_items_filler(item_pool: list, world: World, multiworld: MultiW
 
     for item in item_pool:
         if (shop_shuffle_aira == False and "Aira Item" in world.item_name_to_item[item.name].get("category", [])):
-            logging.info("<Chantelise-Manual> (before_create_items_filler) FOUND - AIRA ITEM TO REMOVE: "+item.name)
+            logging.info("<Chantelise-Manual> (before_create_items_filler) FOUND - AIRA ITEM TO REMOVE: " + item.name)
             if not item.name in aira_list:
                 logging.info("<Chantelise-Manual> (before_create_items_filler) FIRST ONE!")
                 aira_list.append(item.name)
         if shop_shuffle_merchant == False and "Merchant Item" in world.item_name_to_item[item.name].get("category", []):
-            logging.info("<Chantelise-Manual> (before_create_items_filler) FOUND - MERCHANT ITEM TO REMOVE: "+item.name)
+            logging.info("<Chantelise-Manual> (before_create_items_filler) FOUND - MERCHANT ITEM TO REMOVE: " + item.name)
             if not item.name in merchant_list:
                 logging.info("<Chantelise-Manual> (before_create_items_filler) FIRST ONE!")
                 merchant_list.append(item.name)
         if shop_shuffle_survival == False and "Survival Dungeon Item" in world.item_name_to_item[item.name].get("category", []):
-            logging.info("<Chantelise-Manual> (before_create_items_filler) FOUND - SD ITEM TO REMOVE: "+item.name)
+            logging.info("<Chantelise-Manual> (before_create_items_filler) FOUND - SD ITEM TO REMOVE: " + item.name)
             if not item.name in survival_list:
                 logging.info("<Chantelise-Manual> (before_create_items_filler) FIRST ONE!")
                 survival_list.append(item.name)
         if (trade_shuffle == False) and ("Trading Reward" in world.item_name_to_item[item.name].get("category", [])):
-            logging.info("<Chantelise-Manual> (before_create_items_filler) FOUND - TRADE REWARD TO REMOVE: "+item.name)
+            logging.info("<Chantelise-Manual> (before_create_items_filler) FOUND - TRADE REWARD TO REMOVE: " + item.name)
             if not item.name in trade_list:
                 logging.info("<Chantelise-Manual> (before_create_items_filler) FIRST ONE!")
                 trade_list.append(item.name)
 
         # if progressive_equipment and "Standalone Equipment" in world.item_name_to_item[item.name].get("category", []):
-        #     logging.info("<Chantelise-Manual> (before_create_items_filler) FOUND A SOLO EQUIP TO REMOVE: "+item.name)
+        #     logging.info("<Chantelise-Manual> (before_create_items_filler) FOUND A SOLO EQUIP TO REMOVE: " + item.name)
         #     if not item.name in prog_equip_list:
         #         prog_equip_list.append(item.name)
         #         item.count = 1
@@ -154,16 +154,16 @@ def before_create_items_filler(item_pool: list, world: World, multiworld: MultiW
         #     prog_equip_list.append("")
 
     for itemName in aira_list:
-        logging.info("<Chantelise-Manual> (before_create_items_filler) Adding Aira Item for removal: "+ itemName)
+        logging.info("<Chantelise-Manual> (before_create_items_filler) Adding Aira Item for removal: " + itemName)
         itemNamesToRemove.append(itemName)
     for itemName in merchant_list:
-        logging.info("<Chantelise-Manual> (before_create_items_filler) Adding Merchant Item for removal: "+ itemName)
+        logging.info("<Chantelise-Manual> (before_create_items_filler) Adding Merchant Item for removal: " + itemName)
         itemNamesToRemove.append(itemName)
     for itemName in survival_list:
-        logging.info("<Chantelise-Manual> (before_create_items_filler) Adding SD Item for removal: "+ itemName)
+        logging.info("<Chantelise-Manual> (before_create_items_filler) Adding SD Item for removal: " + itemName)
         itemNamesToRemove.append(itemName)
     for itemName in trade_list:
-        logging.info("<Chantelise-Manual> (before_create_items_filler) Adding Trade Item for removal: "+ itemName)
+        logging.info("<Chantelise-Manual> (before_create_items_filler) Adding Trade Item for removal: " + itemName)
         itemNamesToRemove.append(itemName)
     # for itemName in prog_equip_list:
     #     itemNamesToRemove.append(itemName)
@@ -174,7 +174,7 @@ def before_create_items_filler(item_pool: list, world: World, multiworld: MultiW
     # to the list multiple times if you want to remove multiple copies of it.
 
     for itemName in itemNamesToRemove:
-        logging.info("<Chantelise-Manual> (before_create_items_filler) Gonna Remove: "+itemName)
+        logging.info("<Chantelise-Manual> (before_create_items_filler) Gonna Remove: " + itemName)
         item = next(i for i in item_pool if i.name == itemName)
         remove_specific_item(item_pool, item)
 
@@ -263,7 +263,7 @@ def after_set_rules(world: World, multiworld: MultiWorld, player: int):
     # location.access_rule = Example_Rule
 
     if goal == GOAL_ID_CURE_ELISE:
-        trophy_location = world.get_location("Cure Elise with Blue Rose Petals", player)
+        trophy_location = world.get_location("Cure Elise with Blue Rose Petals")
         #trophy_location.access_rule = lambda state: hasEnoughTrophies(state)
         trophy_location.access_rule = lambda state: state.has("Blue Rose Petal", player, get_option_value(multiworld, player, "trophies_required"))
 
